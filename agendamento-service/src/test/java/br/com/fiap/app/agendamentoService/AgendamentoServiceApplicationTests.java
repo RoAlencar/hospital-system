09d29f5d2cef5729bde7074da0f996aa6ab6a91c
Rodrigo de Alencar Xavier <roalencarx@gmail.com>
2026-02-18 10:06:46 -0300

    Initial commit - microservices structure

---
+++ agendamento-service/src/test/java/br/com/fiap/app/agendamentoService/AgendamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.app.agendamentoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
